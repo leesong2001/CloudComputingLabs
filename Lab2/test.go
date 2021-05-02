3e79d7559e41cd9669f2e9ba00a440426e472f1a
skyRiny <1368350749@qq.com>
2021-05-02 22:33:04 +0800

    v-0.9

--- Lab2/test.go
+++ Lab2/test.go
@@ -3,22 +3,32 @@ package main
 import (
 	"fmt"
 	"net"
+	"time"
 )
 
+const TCPTimeLimit = 30 //seconds
 // 每一个协程的处理，现在只是简单打印conn
 func SingleThreadWork(c1 chan net.Conn) {
 	for {
 		conn := <-c1
-		buf := make([]byte, 1024)
-		conn.Read(buf) // 从conn中读取客户端发送的数据内容
-		fmt.Println(string(buf))
+		conn.SetDeadline(time.Now().Add(time.Duration(30) * time.Second))
+		for {
+			fmt.Println(conn.RemoteAddr().String())
+			buf := make([]byte, 1024)
+			_, err := conn.Read(buf) // 从conn中读取客户端发送的数据内容
+			if err != nil {
+				fmt.Println("exit?")
+				break
+			}
+			fmt.Println(string(buf))
+		}
+		conn.Close()
 	}
 }
 
 const ThreadNum = 5
 
 func main() {
-
 	listen, err := net.Listen("tcp", ":8888") // 创建用于监听的 socket
 	if err != nil {
 		fmt.Println("listen err=", err)
@@ -41,6 +51,7 @@ func main() {
 		}
 		// 这里准备起一个协程，为客户端服务
 		//go accept_request_thread(conn)
+		//net.Dial("tcp","http://127.0.0.1:8888/api/camera/get_ptz?camera_id=1324566666789876543")
 		connchan <- conn
 	}
 }
